3259d51d20010c2eadc8e34cd0577f37d892e03c
mercurialHg <radumircea88@gmail.com>
2017-08-17 17:22:02 +0300

    new section

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,51 @@
-$(document).ready(function() {
-    
-// hamburger events
-    $("button.hamburger").click(function() {
+$(document).ready(function () {
+
+    // hamburger events
+    $("button.hamburger").click(function () {
         if ($(".navbar-collapse").hasClass("collapsing")) return;
         $(this).toggleClass("is-active")
-    })
- 
+    });
 
-    // let $autoplaySpeed = 3000;
-    // let $transitionSpeed = 300;
-    // $(".hero-slider").slick({
-    //     arrows: false,
-    //     autoplay: true,
-    //     autoplaySpeed: $autoplaySpeed,
-    //     transitionSpeed: $transitionSpeed,
-    // });
-})
+    $(".slick-carousel").slick({
+        infinite: true,
+        nextArrow: '<div class="slick-arrow slick-next"></div>',
+        prevArrow: '<div class="slick-arrow slick-prev"></div>',
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1220,
+                settings: {
+                    slidesToScroll: 4,
+                    slidesToShow: 4,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 980,
+                settings: {
+                    slidesToScroll: 3,
+                    slidesToShow: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToScroll: 2,
+                    slidesToShow: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: "unslick",
+            },
+        ]
+    });
+
+    $(".favourite").click(function(event){
+        event.preventDefault();
+        $(this).toggleClass("active");
+    })
+})  
